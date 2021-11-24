13884821a33de094cb49a6a126cf9365e141b159
RoySamson-stack <rsam9009@gmail.com>
2021-11-24 12:06:58 +0300

    adding the hrader for the project

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img src="../img/trollface.png" alt="" className="header__logo" />
+      <h1>Meme Generator</h1>
+    </header>
+  )
+}
