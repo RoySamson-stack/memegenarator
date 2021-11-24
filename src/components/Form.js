906732c3b69f93bfca0c2081168f97ce21c357fc
RoySamson-stack <rsam9009@gmail.com>
2021-11-24 19:49:59 +0300

    adding the random generator for the images

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,32 @@
 import React from "react";
+import Memes from "../components/memeData"
 
 export default function Form(){
+
+  let url;
+
+function getMeme(){
+  const meme = Memes.data.memes
+  const randomNumber = Math.floor(Math.random() * meme.length);
+   url = meme[randomNumber].url;
+  return url;
+}
+
+
   return(
     <div className="">
       <form className="form">
         <div className="form_group">
         <input type="text" placeholder="Shut up" className="form_input"/>
-        <input type="text" placeholder="and take my monry" className="form_input"/>
+        <input type="text" placeholder="and take my money" className="form_input"/>
         </div>
-        <button className="form_btn">Get a new Image</button>
+        <button className="form_btn"
+        onClick={getMeme}
+        >Get a new meme image 🖼</button>
       </form>
+      <div className="meme-component">
+
+      </div>
     </div>
   )
 }
